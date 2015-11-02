aeed994547b800fd118570951a47a1afb75693a5
Marcos Santos <meliehl@outlook.com>
2015-11-02 21:25:44 -0200

    WepApi e Camada de Dominio simples - COmmit Inicial

---
+++ src/Domain/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Cliente
+    {
+        internal Cliente()
+        {
+        }
+
+        public Cliente(string Nome,
+            string Email)
+        {
+            this.Id = Guid.NewGuid();
+
+            this.Nome = Nome;
+            this.Email = Email;
+        }
+
+        public Guid Id { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string Email { get; private set; }
+    }
+}
---
+++ src/WebApi/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {            
+            app.UseIISPlatformHandler();
+
+            app.UseMvc();
+        }
+    }
+}
