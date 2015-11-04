ef83d53c5fdf6061659c82906671919ad0cccae0
Marcos Santos <meliehl@outlook.com>
2015-11-04 18:50:25 -0200

    Test off

---
+++ src/Test.Domain/Entities/PersonTest.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Xunit;
+
+namespace Test.Domain.Entities
+{
+    public class PersonTest
+    {
+        private Person CreateTestObject()
+        {
+            return new Person("Marcos Eliehl dos Santos", "meliehl@outlook.com");
+        }
+
+        [Fact]
+        public void ShouldCreateValidObject()
+        {
+            var actual = CreateTestObject();
+            Assert.True(!string.IsNullOrWhiteSpace(actual.Name));
+            Assert.True(!string.IsNullOrWhiteSpace(actual.Email));
+        }
+
+        [Fact]
+        public void ShouldCreateValidObjectAndChangeName()
+        {
+            var actual = CreateTestObject();
+            var expected = "Marcolino";
+            actual.ChangeName("Marcolino");
+            Assert.Equal(expected,actual.Name);
+            
+        }
+
+    }
+}
