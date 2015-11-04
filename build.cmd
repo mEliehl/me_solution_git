9c56e661bd678c482a7d9591985f4022b6274c52
Marcos Santos <meliehl@outlook.com>
2015-11-04 21:21:22 -0200

    teste off

--- build.cmd
+++ build.cmd
@@ -1,71 +1,40 @@
-@ECHO OFF
+@echo off
+cd %~dp0
 
-PUSHD %~dp0
+SETLOCAL
+SET NUGET_VERSION=latest
+SET CACHED_NUGET=%LocalAppData%\NuGet\nuget.%NUGET_VERSION%.exe
+SET BUILDCMD_KOREBUILD_VERSION=
+SET BUILDCMD_DNX_VERSION=
 
-:dnvminstall
-SETLOCAL EnableDelayedExpansion 
-where dnvm
-IF %ERRORLEVEL% neq 0 (
-    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
-        SET PATH=!PATH!;!userprofile!\.dnx\bin
-            SET DNX_HOME=!USERPROFILE!\.dnx
-                GOTO install
-                )
+IF EXIST %CACHED_NUGET% goto copynuget
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/%NUGET_VERSION%/nuget.exe' -OutFile '%CACHED_NUGET%'"
 
-:install
-CALL dnvm install 1.0.0-beta8 -r CoreCLR
-CALL dnvm install 1.0.0-beta8 -r CLR
-CALL dnvm use 1.0.0-beta8 -r CoreCLR
+:copynuget
+IF EXIST .nuget\nuget.exe goto restore
+md .nuget
+copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 :restore
-CALL dnu restore src\Domain
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
+IF EXIST packages\Sake goto getdnx
+IF "%BUILDCMD_KOREBUILD_VERSION%"=="" (
+    .nuget\nuget.exe install KoreBuild -ExcludeVersion -Source https://www.myget.org/F/aspnetvnext/api/v2 -o packages -nocache -pre
+) ELSE (
+    .nuget\nuget.exe install KoreBuild -version -Source https://www.myget.org/F/aspnetvnext/api/v2 %BUILDCMD_KOREBUILD_VERSION% -ExcludeVersion -o packages -nocache -pre
+)
+.nuget\NuGet.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
 
-CALL dnu restore src\Infra
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
+:getdnx
+IF "%BUILDCMD_DNX_VERSION%"=="" (
+    SET BUILDCMD_DNX_VERSION=latest
+)
+IF "%SKIP_DNX_INSTALL%"=="" (
+    CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CoreCLR -arch x86 -alias default
+    CALL packages\KoreBuild\build\dnvm install default -runtime CLR -arch x86 -alias default
+) ELSE (
+    CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
+)
 
-CALL dnu restore src\Test.Domain
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-CALL dnu restore src\Test.Infra
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-CALL dnu restore src\WebApi
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-:pack
-SETLOCAL ENABLEEXTENSIONS
-IF ERRORLEVEL 1 ECHO Unable to enable extensions
-IF DEFINED APPVEYOR_BUILD_NUMBER (SET DNX_BUILD_VERSION=%APPVEYOR_BUILD_NUMBER%) ELSE (SET DNX_BUILD_VERSION=1)
-ECHO DNX_BUILD_VERSION=%DNX_BUILD_VERSION%
-
-CALL dnu pack src\Domain --configuration Release --out artifacts\packages
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-CALL dnu pack src\Infra --configuration Release --out artifacts\packages
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-CALL dnu publish src\Test.Domain --out artifacts\endUserPortal --runtime active
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-CALL dnu publish src\Test.Infra --out artifacts\endUserPortal --runtime active
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-CALL dnu publish src\WebApi --out artifacts\endUserPortal --runtime active
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-CALL 7z a endUserPortal.zip %APPVEYOR_BUILD_FOLDER%\artifacts\endUserPortal\*
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-:test
-CALL dnvm use 1.0.0-beta8 -r CoreCLR
-
-CALL dnx src\Test.Domain test -xml .\xunit-results.xml
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-CALL dnvm use 1.0.0-beta8 -r CoreCLR
-
-CALL dnx src\Test.Infra test -xml .\xunit-results-core.xml
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-
-POPD
+packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
