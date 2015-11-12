3d68bd1a6d520f4df0a383d21dc09c61c3e52043
Marcos Santos <meliehl@outlook.com>
2015-11-12 21:36:07 -0200

    move tests to base class

--- test/Infra.Test/Repositories/BaseRepositoryTest.cs
+++ test/Infra.Test/Repositories/BaseRepositoryTest.cs
@@ -3,6 +3,7 @@ using Domain.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Infra.Test.Repositories
@@ -20,12 +21,13 @@ namespace Infra.Test.Repositories
 
         protected abstract Tentity CreateEntityToTest();
 
+        protected abstract void ChangeEntity(Tentity entity);
+
+        #region Tests
         [Fact]
         public void ShouldAddAndHasValidGuidTest()
         {
-            var actual = CreateEntityToTest();
-            repository.Add(actual);
-            repository.SaveChanges();
+            var actual = CreateOneEntityAddSaveChanges();
             Assert.True(actual.Id != Guid.Empty);
         }
 
@@ -41,10 +43,116 @@ namespace Infra.Test.Repositories
         [Fact]
         public void ShouldAddAndAnyReturnTrueTest()
         {
+            var actual = CreateOneEntityAddSaveChanges();
+            Assert.True(repository.Any(a => a.Id == actual.Id));
+        }
+
+        [Fact]
+        public async Task ShouldAddAndAnyAsyncReturnTrueTest()
+        {
+            var actual = CreateEntityToTest();
+            repository.Add(actual);
+            await repository.SaveChangesAsync();
+            var expect = repository.AnyAsync(a => a.Id == actual.Id);
+            Assert.True(await expect);
+        }
+
+        [Fact]
+        public void ShouldAddAndGetReturnSameEntityTest()
+        {
+            var actual = CreateOneEntityAddSaveChanges();
+            var expected = repository.Get(w => w.Id == actual.Id).FirstOrDefault();
+            Assert.True(actual.Id == expected.Id);
+        }
+
+        [Fact]
+        public async Task ShouldAddAndGetAsyncReturnSameEntityTest()
+        {
+            var actual = CreateOneEntityAddSaveChanges();
+            var expected = await repository.GetAsync(w => w.Id == actual.Id);
+            Assert.True(actual.Id == expected.FirstOrDefault().Id);
+        }
+
+        [Fact]
+        public void ShouldAddAndGetByIdReturnSameEntityTest()
+        {
+            var actual = CreateOneEntityAddSaveChanges();
+            var expected = repository.GetById(actual.Id);
+            Assert.True(actual.Id == expected.Id);
+        }
+
+        [Fact]
+        public async Task ShouldAddAndGetByIdAsyncReturnSameEntityTest()
+        {
+            var actual = CreateOneEntityAddSaveChanges();
+            var expected = await repository.GetByIdAsync(actual.Id);
+            Assert.True(actual.Id == expected.Id);
+        }
+
+        [Fact]
+        public void ShouldAddAndRemoveByIdReturnNoEntityTest()
+        {
+            var actual = CreateOneEntityAddSaveChanges();
+            repository.Remove(actual.Id);
+            repository.SaveChanges();
+            Assert.True(!repository.Any(a => a.Id == actual.Id));
+        }
+
+        [Fact]
+        public void ShouldAddAndRemoveReturnNoEntityTest()
+        {
+            var actual = CreateOneEntityAddSaveChanges();
+            repository.Remove(actual);
+            repository.SaveChanges();
+            Assert.True(!repository.Any(a => a.Id == actual.Id));
+        }
+
+        [Fact]
+        public void ShouldAddAndRemoveRangeReturnNoEntityTest()
+        {
+            IEnumerable<Tentity> actual = new List<Tentity>() { CreateEntityToTest(), CreateEntityToTest() };
+            repository.AddRange(actual);
+            repository.SaveChanges();
+            repository.RemoveRange(actual);
+            repository.SaveChanges();
+            Assert.True(!repository.Any(a => actual.All(a1 => a1.Id == a.Id)));
+        }
+
+        [Fact]
+        public void ShouldAddAndUpdateChangePropertieTest()
+        {
+            var actual = CreateOneEntityAddSaveChanges();
+            ChangeEntity(actual);
+            repository.Update(actual);
+            repository.SaveChanges();
+            var expected = repository.GetById(actual.Id);
+            Assert.Equal(expected.ToString(), actual.ToString());
+        }
+
+        [Fact]
+        public void ShouldAddAndUpdateRangeChangePropertiesTest()
+        {
+            IEnumerable<Tentity> actual = new List<Tentity>() { CreateEntityToTest(), CreateEntityToTest() };
+            repository.AddRange(actual);
+            repository.SaveChanges();
+
+            ChangeEntity(actual.FirstOrDefault());
+            repository.UpdateRange(actual);
+            repository.SaveChanges();
+
+            var expected = repository.GetById(actual.FirstOrDefault().Id);
+            Assert.Equal(expected.ToString(), actual.FirstOrDefault().ToString());
+        }
+        #endregion
+
+        #region Helper Methods
+        private Tentity CreateOneEntityAddSaveChanges()
+        {
             var actual = CreateEntityToTest();
             repository.Add(actual);
             repository.SaveChanges();
-            Assert.True(repository.Any(a => a.Id == actual.Id));
+            return actual;
         }
+        #endregion
     }
 }
--- test/Infra.Test/Repositories/PersonRepositoryTests.cs
+++ test/Infra.Test/Repositories/PersonRepositoryTests.cs
@@ -1,11 +1,6 @@
 ﻿using Domain.Entities;
 using Domain.Repositories;
 using Infra.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace Infra.Test.Repositories
 {
@@ -24,112 +19,9 @@ namespace Infra.Test.Repositories
             return new Person("Marcos Eliehl dos Santos", "meliehl@outlook.com");
         }
 
-        [Fact]
-        public async Task ShouldAddAndAnyAsyncReturnTrueTest()
+        protected override void ChangeEntity(Person entity)
         {
-            var actual = CreateEntityToTest();
-            personRepository.Add(actual);
-            await personRepository.SaveChangesAsync();
-            var expect = personRepository.AnyAsync(a => a.Id == actual.Id);
-            Assert.True(await expect);
-        }
-
-        [Fact]
-        public void ShouldAddAndGetReturnSameEntityTest()
-        {
-            var actual = CreateEntityToTest();
-            personRepository.Add(actual);
-            personRepository.SaveChanges();
-            var expected = personRepository.Get(w => w.Id == actual.Id).FirstOrDefault();
-            Assert.True(actual.Id == expected.Id);
-        }
-
-        [Fact]
-        public async Task ShouldAddAndGetAsyncReturnSameEntityTest()
-        {
-            var actual = CreateEntityToTest();
-            personRepository.Add(actual);
-            personRepository.SaveChanges();
-            var expected = await personRepository.GetAsync(w => w.Id == actual.Id);
-            Assert.True(actual.Id == expected.FirstOrDefault().Id);
-        }
-
-        [Fact]
-        public void ShouldAddAndGetByIdReturnSameEntityTest()
-        {
-            var actual = CreateEntityToTest();
-            personRepository.Add(actual);
-            personRepository.SaveChanges();
-            var expected = personRepository.GetById(actual.Id);
-            Assert.True(actual.Id == expected.Id);
-        }
-
-        [Fact]
-        public async Task ShouldAddAndGetByIdAsyncReturnSameEntityTest()
-        {
-            var actual = CreateEntityToTest();
-            personRepository.Add(actual);
-            personRepository.SaveChanges();
-            var expected = await personRepository.GetByIdAsync(actual.Id);
-            Assert.True(actual.Id == expected.Id);
-        }
-
-        [Fact]
-        public void ShouldAddAndRemoveByIdReturnNoEntityTest()
-        {
-            var actual = CreateEntityToTest();
-            personRepository.Add(actual);
-            personRepository.SaveChanges();
-            personRepository.Remove(actual.Id);
-            personRepository.SaveChanges();
-            Assert.True(!personRepository.Any(a => a.Id == actual.Id));
-        }
-
-        [Fact]
-        public void ShouldAddAndRemoveReturnNoEntityTest()
-        {
-            var actual = CreateEntityToTest();
-            personRepository.Add(actual);
-            personRepository.SaveChanges();
-            personRepository.Remove(actual);
-            personRepository.SaveChanges();
-            Assert.True(!personRepository.Any(a => a.Id == actual.Id));
-        }
-
-        [Fact]
-        public void ShouldAddAndRemoveRangeReturnNoEntityTest()
-        {
-            var actual = new List<Person>() { CreateEntityToTest(), CreateEntityToTest() };
-            personRepository.AddRange(actual);
-            personRepository.SaveChanges();
-            personRepository.RemoveRange(actual);
-            personRepository.SaveChanges();
-            Assert.True(!personRepository.Any(a => actual.All(a1 => a1.Id == a.Id)));
-        }
-
-        [Fact]
-        public void ShouldAddAndUpdateChangePropertieTest()
-        {
-            var actual = CreateEntityToTest();
-            personRepository.Add(actual);
-            personRepository.SaveChanges();
-            actual.ChangeName("Marcolino");
-            personRepository.Update(actual);
-            personRepository.SaveChanges();
-            Assert.True(personRepository.Any(a => a.Name == actual.Name));
-        }
-
-        [Fact]
-        public void ShouldAddAndUpdateRangeChangePropertiesTest()
-        {
-            var actual = new List<Person>() { CreateEntityToTest(), CreateEntityToTest() };
-            personRepository.AddRange(actual);
-            personRepository.SaveChanges();
-
-            actual.FirstOrDefault().ChangeName("Marcolino");
-            personRepository.UpdateRange(actual);
-            personRepository.SaveChanges();
-            Assert.True(personRepository.Any(a => actual.Any(a1 => a1.Name == a.Name)));
+            entity.ChangeName("Marcolino");
         }
     }
 }
